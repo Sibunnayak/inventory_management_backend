fcdc6755c17fa128525d735b940d9405b54499f5
Sibunnayak <sibunnayak8480@gmail.com>
2024-09-20 13:41:32 +0530

    update the origin access

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
     origin: [
         'http://localhost:5173', 
-        'https://inventory-management-frontend-cysl.onrender.com'
+        'https://inventory-management-frontend-1-wqvq.onrender.com'
     ]
 }));
 
